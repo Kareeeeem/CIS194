5e635d08d4cd90f555c41b9d8dbf5dd3db9a2ecf
kareeeeem <kareeeeem@gmail.com>
2015-05-17 18:52:18 +0200

    sieve of sundaram
    With an extra solution found online that I found interesting.

--- week4/hw.hs
+++ week4/hw.hs
@@ -1,5 +1,7 @@
 module Week4 where
 
+import Data.List
+
 fun1' :: [Integer] -> Integer
 fun1' = product . map (subtract 2) . filter even
 
@@ -30,3 +32,27 @@ xor = foldr1 (/=)
 
 map' :: (a -> b) -> [a] -> [b]
 map' f = foldr (\x acc -> f x:acc) []
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram x = map ((+1) . (*2)) ([1..x] \\ sieve)
+  where sieve = takeWhile (<=x) [ i+j+2*i*j | i <- [1..], j <- [i..] ]
+
+-- Found this online, should be interesting to study this solution.
+-- http://decomputed.com/post/110883687867/sieves-in-haskell-part-1
+initialSundaramSieve :: Integral t => t -> [t]
+initialSundaramSieve limit =
+    let topi = floor (sqrt ((fromIntegral limit) / 2))
+     in [i + j + 2*i*j | i <- [1..topi],
+     j <- [i..floor((fromIntegral(limit-i)) / fromIntegral(2*i+1))]]
+
+sundaram5 :: Integral a => a -> [a]
+sundaram5 limit =
+    let halfLimit = (limit `div` 2)
+     in 2:removeComposites ([1..halfLimit]) (sort $ initialSundaramSieve halfLimit)
+
+removeComposites :: (Num a, Ord a) => [a] -> [a] -> [a]
+removeComposites []     _                  = []
+removeComposites sieve  []                 = sieve
+removeComposites (s:ss) (c:cs) | s == c    = removeComposites ss cs
+  | s > c     = removeComposites (s:ss) cs
+  | otherwise = 2*s+1 : (removeComposites ss (c:cs))
